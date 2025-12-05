a3c1533bce1f4cdb6fbf63215d419bf9095d8079
Harunzy <Harunzy55@gmail.com>
2025-12-05 11:58:38 +0000

    add variables

---
+++ day-1/variable.js
@@ -0,0 +1,13 @@
+//can be reasign
+let name = 'Haruna';
+
+//cannot be reasign
+const age = 26;
+
+name = 'Zakaria';
+//define only
+let yourName;
+
+console.log(name); //output zakaria instead of Haruna
+
+console.log(yourName); //undefine
