a3cb922bcf79326a43f6869ef0543355f9d88239
Harunzy <Harunzy55@gmail.com>
2025-12-06 23:13:58 +0000

    add

---
+++ Guest/index.js
@@ -0,0 +1,8 @@
+const name = 'Dauda';
+const guestList = ['Dauda', 'Aish', 'Hamis', 'Suale'];
+
+if (guestList.includes(name)) {
+  console.log('Welcome to the party');
+} else {
+  console.log('You are not welcome to this party');
+}
