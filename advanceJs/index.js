a63679993689cb567c337bbdce3d36399d601e66
Harunzy <Harunzy55@gmail.com>
2025-12-06 12:04:14 +0000

    add conditional rendering

--- advanceJs/index.js
+++ advanceJs/index.js
@@ -5,9 +5,9 @@ form.addEventListener('submit', function (event) {
   const yourName = document.querySelector('#yourName').value;
   const partnerName = document.querySelector('#partnetName').value;
 
-  let n = Math.random();
+  let loveScore = Math.random();
 
-  n = Math.floor(n * 100) + 1;
+  loveScore = Math.floor(loveScore * 100) + 1;
   const Compatibility = [
     '❤️ Low Compatibility',
     '💞 Medium Compatibility',
@@ -15,12 +15,12 @@ form.addEventListener('submit', function (event) {
   ];
   //const message = `${yourName} , Your Love with ${partnerName} is ${n}%`;
   let message;
-  if (n <= 50) {
-    message = `${yourName} , Your Love with ${partnerName} is ${n}% ${Compatibility[0]}`;
-  } else if (n > 50 && n <= 75) {
-    message = `${yourName} , Your Love with ${partnerName} is ${n}% ${Compatibility[1]} `;
+  if (loveScore <= 50) {
+    message = `${yourName} , Your Love with ${partnerName} is ${loveScore}% ${Compatibility[0]}. Love has potential, but it needs nurturing. This connection may face challenges, yet with patience, communication, and effort, something beautiful can still grow.`;
+  } else if (loveScore > 50 && loveScore <= 75) {
+    message = `${yourName} , Your Love with ${partnerName} is ${loveScore}% ${Compatibility[1]}. There’s definitely a spark between you two! With consistency, honesty, and a little adventure, this relationship can blossom into something meaningful. `;
   } else {
-    message = `${yourName} , Your Love with ${partnerName} is ${n}% ${Compatibility[2]}`;
+    message = `${yourName} , Your Love with ${partnerName} is ${loveScore}% ${Compatibility[2]}. A powerful bond! Your hearts align with ease, and your energy complements each other beautifully. This is the kind of connection that can grow into a lasting love story.`;
   }
   para.append(message);
 });
