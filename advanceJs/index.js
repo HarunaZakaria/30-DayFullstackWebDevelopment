34d569a5534c28d89dc6c8f22e3b42356066e5d3
Harunzy <Harunzy55@gmail.com>
2025-12-06 12:44:55 +0000

    append children to a div

--- advanceJs/index.js
+++ advanceJs/index.js
@@ -1,5 +1,7 @@
 const para = document.querySelector('.para');
 const form = document.querySelector('.form');
+const h1 = document.createElement('h1');
+const p = document.createElement('p');
 form.addEventListener('submit', function (event) {
   //event.preventDefault();
   const yourName = document.querySelector('#yourName').value;
@@ -14,13 +16,19 @@ form.addEventListener('submit', function (event) {
     '💖 High Compatibility',
   ];
   //const message = `${yourName} , Your Love with ${partnerName} is ${n}%`;
-  let message;
   if (loveScore <= 50) {
-    message = `${yourName} , Your Love with ${partnerName} is ${loveScore}% ${Compatibility[0]}. Love has potential, but it needs nurturing. This connection may face challenges, yet with patience, communication, and effort, something beautiful can still grow.`;
+    h1.innerHTML = `${yourName} , Your Love with ${partnerName} is ${loveScore}% ${Compatibility[0]}. `;
+    p.innerHTML =
+      'Love has potential, but it needs nurturing. This connection may face challenges, yet with patience, communication, and effort, something beautiful can still grow.';
   } else if (loveScore > 50 && loveScore <= 75) {
-    message = `${yourName} , Your Love with ${partnerName} is ${loveScore}% ${Compatibility[1]}. There’s definitely a spark between you two! With consistency, honesty, and a little adventure, this relationship can blossom into something meaningful. `;
+    h1.innerHTML = `${yourName} , Your Love with ${partnerName} is ${loveScore}% ${Compatibility[1]}.`;
+    p.innerHTML =
+      'There is definitely a spark between you two! With consistency, honesty, and a little adventure, this relationship can blossom into something meaningful. ';
   } else {
-    message = `${yourName} , Your Love with ${partnerName} is ${loveScore}% ${Compatibility[2]}. A powerful bond! Your hearts align with ease, and your energy complements each other beautifully. This is the kind of connection that can grow into a lasting love story.`;
+    h1.innerHTML = `${yourName} , Your Love with ${partnerName} is ${loveScore}% ${Compatibility[2]}. `;
+    p.innerHTML =
+      'A powerful bond! Your hearts align with ease, and your energy complements each other beautifully. This is the kind of connection that can grow into a lasting love story.';
   }
-  para.append(message);
+  para.append(h1);
+  para.append(p);
 });
