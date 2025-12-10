83b4137108b8edc71f8473de5252e1e4a5be2769
Harunzy <Harunzy55@gmail.com>
2025-12-10 16:33:34 +0000

    add a method

--- constractorFunction/index.js
+++ constractorFunction/index.js
@@ -4,9 +4,15 @@ class HouseHelp {
     (this.name = name),
       (this.age = age),
       (this.location = location),
-      this.yearsOfEperience;
+      (this.yearsOfEperience = yearsOfEperience),
+      //greet method
+      (this.greet = function () {
+        console.log(
+          `Hi, my name is ${this.name} and I have ${this.yearsOfEperience} years of experience`
+        );
+      });
   }
 }
 
 const haruna = new HouseHelp('Zakaria', 26, 'Tamale', 6);
-console.log(haruna);
+console.log(haruna.greet());
