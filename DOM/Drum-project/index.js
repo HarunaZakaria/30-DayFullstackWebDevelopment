e03385d42d7fe04090909a9d7becebe7a21018e6
Harunzy <Harunzy55@gmail.com>
2025-12-10 17:04:42 +0000

    add keydown event

--- DOM/Drum-project/index.js
+++ DOM/Drum-project/index.js
@@ -2,41 +2,48 @@ const drumButtons = document.querySelectorAll('.drum');
 drumButtons.forEach((button) => {
   button.addEventListener('click', () => {
     const buttonInnerHTML = button.innerHTML;
-    switch (buttonInnerHTML) {
-      case 'w':
-        const tom1 = new Audio('sounds/tom-1.mp3');
-        tom1.play();
-        break;
-      case 'a':
-        const tom2 = new Audio('sounds/tom-2.mp3');
-        tom2.play();
-        break;
-      case 's':
-        const tom3 = new Audio('sounds/tom-3.mp3');
-        tom3.play();
-        break;
-      case 'd':
-        const tom4 = new Audio('sounds/tom-4.mp3');
-        tom4.play();
-        break;
-      case 'j':
-        const snare = new Audio('sounds/snare.mp3');
-        snare.play();
-        break;
-      case 'k':
-        const kick = new Audio('sounds/crash.mp3');
-        kick.play();
-        break;
-      case 'l':
-        const crash = new Audio('sounds/kick-bass.mp3');
-        crash.play();
-        break;
-      default:
-        console.log(buttonInnerHTML);
-        break;
-    }
+    makeSound(buttonInnerHTML);
   });
 });
 
+document.addEventListener('keydown', (e) => {
+  makeSound(e.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case 'w':
+      const tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case 'a':
+      const tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case 's':
+      const tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case 'd':
+      const tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case 'j':
+      const snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case 'k':
+      const kick = new Audio('sounds/crash.mp3');
+      kick.play();
+      break;
+    case 'l':
+      const crash = new Audio('sounds/kick-bass.mp3');
+      crash.play();
+      break;
+    default:
+      console.log(key);
+      break;
+  }
+}
 // let audio = new Audio('sounds/tom-1.mp3');
 //     audio.play();
