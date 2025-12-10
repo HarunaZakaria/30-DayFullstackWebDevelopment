e8908407157c1835a3114868cfed043b860c05b0
Harunzy <Harunzy55@gmail.com>
2025-12-10 02:59:41 +0000

    add a constractor function

---
+++ constractorFunction/index.js
@@ -0,0 +1,10 @@
+//constractor function of house help
+function HouseHelp(name, age, location, yearsOfEperience) {
+  (this.name = name),
+    (this.age = age),
+    (this.location = location),
+    this.yearsOfEperience;
+}
+
+const haruna = new HouseHelp('Zakaria', 26, 'Tamale', 6);
+console.log(haruna);
