83ec3b2aac2695efeec673a79c5d7bb4113c239a
Harunzy <Harunzy55@gmail.com>
2025-12-11 21:58:23 +0000

    update jquery

--- jquery/index.js
+++ jquery/index.js
@@ -1,1 +1,16 @@
-$('h1').css('color', 'blue');
+// $() or jqueery()
+const h1 = $('h1');
+// h1.css('color', 'blue');
+// h1.css('backgroundColor', 'green');
+
+// adding class in jquery
+h1.addClass('big-heading');
+
+//removing class in jquery
+h1.removeClass('big-heading');
+
+//changing text in jquery using text method
+h1.text('Haruna');
+
+//changing text in jquery using html
+h1.html('Zakaria');
