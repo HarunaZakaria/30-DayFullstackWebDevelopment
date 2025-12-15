e5b431863d4056afe378e18b3eb641399b577b5f
Harunzy <Harunzy55@gmail.com>
2025-12-15 13:47:18 +0000

    add

--- jquery/simonGame/game.js
+++ jquery/simonGame/game.js
@@ -1,23 +1,33 @@
-const gamePattern = [];
 const buttonColours = ['red', 'blue', 'green', 'yellow'];
+
+const gamePattern = [];
 const userClickedPattern = [];
 
-$('button').click(function () {
+$('.btn').click(function () {
   const userChosenColour = $(this).attr('id');
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
+
+  //1. In the same way we played sound in nextSequence() , when a user clicks on a button, the corresponding sound should be played.
+  playSound(userChosenColour);
 });
 
 function nextSequence() {
   const randomNumber = Math.floor(Math.random() * 4);
   const randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
   $('#' + randomChosenColour)
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
 
-  const audio = new Audio('sounds/' + randomChosenColour + '.mp3');
+  //4. Refactor the code in playSound() so that it will work for both playing sound in nextSequence() and when the user clicks a button.
+  playSound(randomChosenColour);
+}
+
+//2. Create a new function called playSound() that takes a single input parameter called name.
+function playSound(name) {
+  //3. Take the code we used to play sound in the nextSequence() function and add it to playSound().
+  const audio = new Audio('sounds/' + name + '.mp3');
   audio.play();
-  console.log(audio);
 }
