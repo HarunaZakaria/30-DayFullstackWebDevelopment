7e7a1754cf41f6cc5a03fb918a5a4edd5bf1a898
Harunzy <Harunzy55@gmail.com>
2025-12-17 22:33:12 +0000

    add npm packages

--- BackendDev/Node/index.js
+++ BackendDev/Node/index.js
@@ -1,13 +1,22 @@
 //creating a file in ur local machine using node
 import { error } from 'console';
 import fs from 'fs';
-fs.writeFile('name.txt', 'Haruna', (error) => {
-  if (error) throw error;
-  console.log('file created successful');
-});
+import { randomSuperhero } from 'superheroes';
+import { randomSupervillain } from 'supervillains';
+import sillyname from 'sillyname';
+const myHero = randomSuperhero();
+const myVillain = randomSupervillain();
+const name = sillyname();
+console.log(myHero);
+console.log(myVillain);
+console.log(name);
+// fs.writeFile('name.txt', 'Haruna', (error) => {
+//   if (error) throw error;
+//   console.log('file created successful');
+// });
 
 //read a file
-fs.readFile('./name.txt', 'utf8', (error, data) => {
-  if (error) throw error;
-  console.log(data);
-});
+// fs.readFile('./name.txt', 'utf8', (error, data) => {
+//   if (error) throw error;
+//   console.log(data);
+// });
