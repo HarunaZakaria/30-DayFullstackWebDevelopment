4f7d2d0bf08049b567cfd909350202ae5d57e849
Harunzy <Harunzy55@gmail.com>
2025-12-17 21:23:18 +0000

    writing and reading file

--- BackendDev/Node/index.js
+++ BackendDev/Node/index.js
@@ -1,2 +1,13 @@
-import fs from "fs";
-fs.writeFile()
+//creating a file in ur local machine using node
+import { error } from 'console';
+import fs from 'fs';
+fs.writeFile('name.txt', 'Haruna', (error) => {
+  if (error) throw error;
+  console.log('file created successful');
+});
+
+//read a file
+fs.readFile('./name.txt', 'utf8', (error, data) => {
+  if (error) throw error;
+  console.log(data);
+});
