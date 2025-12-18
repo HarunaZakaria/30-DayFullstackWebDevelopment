801ba93affc91c5b6d6102c5c8b80c654bea5fcc
Harunzy <Harunzy55@gmail.com>
2025-12-18 22:03:15 +0000

    add

--- BackendDev/Express/server/index.js
+++ BackendDev/Express/server/index.js
@@ -4,17 +4,34 @@ const app = express();
 const port = 3000;
 //home
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.send('<h1>Hello world</h1>');
 });
 //sign in
 app.get('/signin', (req, res) => {
-  res.send('Welcome To Sign In Page');
+  res.send('<h1>Welcome To Sign In Page</h1>');
 });
 //sign up
 app.get('/signup', (req, res) => {
   res.send('Welcome To Sign Up Page');
 });
+//diffent HTTP requestss
 
+//post request
+app.post('/register', (req, res) => {
+  res.sendStatus(201);
+});
+//put request
+app.put('/user', (req, res) => {
+  res.sendStatus(200);
+});
+//patch request
+app.patch('/user/haruna', (req, res) => {
+  res.sendStatus(200);
+});
+//delete request
+app.delete('/user/zakaria', (req, res) => {
+  res.sendStatus(200);
+});
 app.listen(port, () => {
   console.log(`Server running on portd ${port}`);
 });
