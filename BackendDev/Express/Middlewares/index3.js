9e4c4d85677b11463ce39a7a07af1971ef5f71ca
Harunzy <Harunzy55@gmail.com>
2025-12-20 01:27:05 +0000

    add a custome middleware

--- BackendDev/Express/Middlewares/index3.js
+++ BackendDev/Express/Middlewares/index3.js
@@ -1,12 +1,19 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 3000;
+//create a custome middleware to log request method and url
+function logger(req, res, next) {
+  console.log(
+    `This is the request metho: ${req.method} and the url is : ${req.url}`
+  );
+  next();
+}
 
 app.use(logger);
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.get('/', (req, res) => {
+  res.send('Hello');
 });
 
 app.listen(port, () => {
