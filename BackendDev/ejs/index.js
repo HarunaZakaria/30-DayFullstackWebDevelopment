a9f2122eec98aef4c6a979848f9fe931a2ad6f27
Harunzy <Harunzy55@gmail.com>
2025-12-21 22:00:10 -0800

    add

--- BackendDev/ejs/index.js
+++ BackendDev/ejs/index.js
@@ -1,11 +1,21 @@
 import express from "express";
 const app = express();
-const port = 5000;
+const port = 3000;
 
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+// Optional: Specify the views directory (default is './views')
+// app.set('views', './views');
+
+// Define a route to render an EJS template
 app.get("/", (req, res) => {
-  res.send("Hello");
+  // Pass data to the template
+  res.render("index", {
+    title: "Hey",
+    message: "Hello from Harunzy!",
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
