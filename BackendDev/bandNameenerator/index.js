bbde72da0af7e020c0541b3e3e65634cb6cb4090
Harunzy <Harunzy55@gmail.com>
2025-12-25 21:47:58 -0800

    add

--- BackendDev/bandNameenerator/index.js
+++ BackendDev/bandNameenerator/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
-app.use(express.static('public'))
+app.use(express.static("public"));
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
 
@@ -152,7 +152,6 @@ const adj = [
   "bogus",
   "boiling",
   "bold",
-  "bony",
   "boring",
   "bossy",
   "both",
