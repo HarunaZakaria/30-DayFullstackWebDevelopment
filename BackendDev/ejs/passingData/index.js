19afc79ad76e3484fd103e0d7bafa90cd98d46b8
Harunzy <Harunzy55@gmail.com>
2025-12-30 22:48:25 -0800

    add all

--- BackendDev/ejs/passingData/index.js
+++ BackendDev/ejs/passingData/index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
@@ -15,6 +16,24 @@ app.post("/submit", (req, res) => {
   res.render("index.ejs", {
     numOfLetters: nameLength,
   });
+});
+
+// API endpoint that returns JSON
+app.post("/api/submit", (req, res) => {
+  try {
+    const nameLength = req.body["fName"].length + req.body["lName"].length;
+    res.json({
+      success: true,
+      numOfLetters: nameLength,
+      fName: req.body["fName"],
+      lName: req.body["lName"]
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message
+    });
+  }
 });
 
 app.listen(port, () => {
--- BackendDev/ejs/passingData/solution.js
+++ BackendDev/ejs/passingData/solution.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render("solution.ejs");
@@ -13,6 +14,24 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
   const numLetters = req.body["fName"].length + req.body["lName"].length;
   res.render("solution.ejs", { numberOfLetters: numLetters });
+});
+
+// API endpoint that returns JSON
+app.post("/api/submit", (req, res) => {
+  try {
+    const numLetters = req.body["fName"].length + req.body["lName"].length;
+    res.json({
+      success: true,
+      numberOfLetters: numLetters,
+      fName: req.body["fName"],
+      lName: req.body["lName"]
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message
+    });
+  }
 });
 
 app.listen(port, () => {
