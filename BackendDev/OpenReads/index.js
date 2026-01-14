8eb5178221316a4756b982e12e854ac9b3f9fcf2
Harunzy <Harunzy55@gmail.com>
2026-01-13 18:56:26 -0800

    add

--- BackendDev/OpenReads/index.js
+++ BackendDev/OpenReads/index.js
@@ -2,7 +2,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
-import e from "express";
 
 //initialize the express app
 const app = express();
@@ -12,8 +11,43 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 //define a route to handle GET requests
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/search", async (req, res) => {
+  const query = req.body.bookName;
+
+  const response = await axios.get(
+    `https://openlibrary.org/search.json?q=${query}`
+  );
+
+  const books = response.data.docs.map((book) => ({
+    title: book.title,
+    author: book.author_name?.[0],
+    year: book.first_publish_year,
+    pdf: book.ia ? `https://openlibrary.org/books/${book.ia}.pdf` : null,
+  }));
+
+  res.json(books);
+});
+//submit a form to search for books
+app.post("/submit", async (req, res) => {
+  const search = req.body.bookName;
+
+  try {
+    const response = await axios.get(
+      `https://openlibrary.org/search.json?q=${search}`
+    );
+    const books = response.data;
+    const title = books.docs[0].title;
+    const author = books.docs[0].author_name;
+    console.log(`Title: ${title}, Author: ${author}`);
+    res.render("index.ejs", { books: books });
+  } catch (error) {
+    console.error("Error fetching data from Google Books API:", error);
+    res.render("index.ejs", { books: [] });
+  }
 });
 //define a route to handle POST requests
 
