cff8818f7484c2e180ff5204fb26ea141a83a6f5
Harunzy <Harunzy55@gmail.com>
2026-03-31 22:35:29 -0700

    add a solution

--- BackendDev/8.2 Postgres Read/index.js
+++ BackendDev/8.2 Postgres Read/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import pg from "pg";
 const app = express();
 const port = 3000;
 
@@ -9,6 +9,24 @@ let totalCorrect = 0;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Harunzy@26",
+  port: 5433,
+});
+db.connect();
+
+let quiz = [];
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
 
 let currentQuestion = {};
 
