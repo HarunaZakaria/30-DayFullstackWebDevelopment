0da03954954676cb5d2efe5c3974307b88129d88
Harunzy <Harunzy55@gmail.com>
2026-04-16 13:44:45 -0700

    add solution

---
+++ React-with-angela/282-javascript-expressions-in-jsx-practice/src/index.jsx
@@ -0,0 +1,23 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+// Solution
+import React from "react";
+import ReactDom from "react-dom";
+
+const fullName = "Zakaria Haruna";
+const currentYear = 2026;
+
+ReactDom.render(
+  <>
+    <p>Created by {fullName}</p>
+    <p>Copyright {currentYear}</p>
+  </>,
+  document.querySelector("#root"),
+);
