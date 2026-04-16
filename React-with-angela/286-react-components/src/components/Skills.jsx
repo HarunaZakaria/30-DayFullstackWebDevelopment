c712d6e4a84a4db89668c48ebdb2b5766324ef26
Harunzy <Harunzy55@gmail.com>
2026-04-16 16:32:28 -0700

    add skills.jsx

---
+++ React-with-angela/286-react-components/src/components/Skills.jsx
@@ -0,0 +1,11 @@
+export default function Skills() {
+  return (
+    <div>
+      <ul>
+        <li>Frontend Development</li>
+        <li>Backend Development</li>
+        <li>UI/UX Development</li>
+      </ul>
+    </div>
+  );
+}
