25d76ec62a5674bcf659ddaec92c8128bfa88b0d
Harunzy <Harunzy55@gmail.com>
2026-04-16 17:32:50 -0700

    add solution

---
+++ React-with-angela/293-keeper-app-part-1-challenge/src/App.jsx
@@ -0,0 +1,12 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Note from "./components/Note";
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
