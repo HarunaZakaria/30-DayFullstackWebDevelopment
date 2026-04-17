1d6851e009172c1423efc4e3857ee4ab422867cd
Harunzy <Harunzy55@gmail.com>
2026-04-17 15:00:35 -0700

    add final solution

--- React-with-angela/299-mapping-components-practice/src/components/App.jsx
+++ React-with-angela/299-mapping-components-practice/src/components/App.jsx
@@ -2,21 +2,36 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
+function createEmojiTerm(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      meaning={emojiTerm.meaning}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      {emojipedia.map((emoji, index) => (
-        <div key={emoji.id}>
-          <Entry
-            emoji={emoji.emoji}
-            name={emoji.name}
-            meaning={emoji.meaning}
-          />
-        </div>
-      ))}
+      <dl className="dictionary">
+        {emojipedia.map(createEmojiTerm)}
+        {/* alternative way to map list of items */}
+        {/* {emojipedia.map((emoji, index) => (
+          <div key={emoji.id}>
+            <Entry
+              emoji={emoji.emoji}
+              name={emoji.name}
+              meaning={emoji.meaning}
+            />
+          </div>
+        ))} */}
+      </dl>
     </div>
   );
 }
