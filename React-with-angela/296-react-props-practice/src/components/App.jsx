f15dbe0f56aa8b0c136f83b87ecb6fa1d8eed4a2
Harunzy <Harunzy55@gmail.com>
2026-04-17 13:37:29 -0700

    add

--- React-with-angela/296-react-props-practice/src/components/App.jsx
+++ React-with-angela/296-react-props-practice/src/components/App.jsx
@@ -1,11 +1,15 @@
 import { contacts } from "../contacts";
 import React from "react";
 import Card from "./Card";
+import Avator from "./Avator";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <div>
+        <Avator img="https://avatars.githubusercontent.com/u/104790819?v=4?s=400" />
+      </div>
       <div>
         {contacts.map((contact, index) => (
           <div key={index}>
