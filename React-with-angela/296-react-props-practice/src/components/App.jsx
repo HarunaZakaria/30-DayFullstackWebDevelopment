fa1f8267f7a247e3f9bf882874f42596280b8fa2
Harunzy <Harunzy55@gmail.com>
2026-04-17 00:43:09 -0700

    add solution

--- React-with-angela/296-react-props-practice/src/components/App.jsx
+++ React-with-angela/296-react-props-practice/src/components/App.jsx
@@ -1,21 +1,22 @@
+import { contacts } from "../contacts";
 import React from "react";
+import Card from "./Card";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
+      <div>
+        {contacts.map((contact, index) => (
+          <div key={index}>
+            <Card
+              name={contact.name}
+              img={contact.imgURL}
+              phone={contact.phone}
+              email={contact.email}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
