9cf70b2bcca241e9ea27b8ed59fe816056d60d9a
Harunzy <Harunzy55@gmail.com>
2026-04-18 16:09:53 -0700

    add solution

--- React-with-angela/307-useState-hook-practice/src/components/App.jsx
+++ React-with-angela/307-useState-hook-practice/src/components/App.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  //1. Given that you can get the current time using:
+  let now = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(now);
+
+  function getNewTime() {
+    const netTime = new Date().toLocaleTimeString();
+    setTime(netTime);
+  }
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={getNewTime}>Get Time</button>
     </div>
   );
 }
