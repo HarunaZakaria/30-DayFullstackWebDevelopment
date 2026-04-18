1a0222fbbbe7821c6e03de35b4d55070a360f545
Harunzy <Harunzy55@gmail.com>
2026-04-18 14:54:08 -0700

    add useState hook

--- React-with-angela/304-conditional-rendering-practice/src/components/App.jsx
+++ React-with-angela/304-conditional-rendering-practice/src/components/App.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 import Admin from "./Admin";
 
-var userIsRegistered = true;
+const userIsRegistered = true;
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+   function diCreaseCount() {
+    setCount(count - 1);
+  }
   return (
     <div className="container">
       {/* <Form isRegistered={userIsRegistered} /> */}
-      <Admin />
+      {/* <Admin /> */}
+      <h1>{count}</h1>
+      <button onClick={increaseCount}>+</button>
+      <button onClick={diCreaseCount}>-</button>
     </div>
   );
 }
