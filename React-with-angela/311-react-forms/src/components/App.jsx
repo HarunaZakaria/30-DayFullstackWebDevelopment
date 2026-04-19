06c26e9605e58901a51c7d0f8cbe47545e96fa2f
Harunzy <Harunzy55@gmail.com>
2026-04-18 18:37:39 -0700

    add solution

--- React-with-angela/311-react-forms/src/components/App.jsx
+++ React-with-angela/311-react-forms/src/components/App.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [headText, setHeadText] = useState("");
+  function changeEvent(e) {
+    setName(e.target.value);
+    //console.log(e.target.value);
+  }
+  function greetName() {
+    setHeadText(name);
+  }
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headText} </h1>
+      <input
+        type="text"
+        placeholder="What's your name?"
+        onChange={changeEvent}
+        value={name}
+      />
+      <button onClick={greetName}>Submit</button>
     </div>
   );
 }
