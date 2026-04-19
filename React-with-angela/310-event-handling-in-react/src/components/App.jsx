fdf7a2f32d9dbccaff35bc44c8e3312b4fc23079
Harunzy <Harunzy55@gmail.com>
2026-04-18 17:45:17 -0700

    add solution

--- React-with-angela/310-event-handling-in-react/src/components/App.jsx
+++ React-with-angela/310-event-handling-in-react/src/components/App.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [isMouseHover, setIsMouseHover] = useState(false)
+  function mouseIn() {
+    setIsMouseHover(true)
+  }
+  function mouseOut() {
+    setIsMouseHover(false)
+  
+  }
   return (
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onMouseOver={mouseIn} onMouseOut={mouseOut} style={{backgroundColor: isMouseHover ? "black": "white"}}>
+        Submit
+      </button>
     </div>
   );
 }
