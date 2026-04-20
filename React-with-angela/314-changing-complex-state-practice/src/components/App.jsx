d876784257e55cef3d87967beec2f246073fd0f0
Harunzy <Harunzy55@gmail.com>
2026-04-20 00:41:07 -0700

    add

--- React-with-angela/314-changing-complex-state-practice/src/components/App.jsx
+++ React-with-angela/314-changing-complex-state-practice/src/components/App.jsx
@@ -1,22 +1,44 @@
 import React, { useState } from "react";
 
 function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
-  });
+  // const [contact, setContact] = useState({
+  //   fName: "",
+  //   lName: "",
+  //   email: "",
+  // });
 
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [updatedFName, setUpdatedFName] = useState("");
+  const [updatedLName, setUpdatedLName] = useState("");
+  const [updatedEmail, setupdatedEmail] = useState("");
+  function updateContact(event) {
+    //event.preventDefault();
+    const [setFName, setLName, setEmail] = event.target.value;
+  }
+
+  function updatContact(e) {
+    e.preventDefault()
+    setUpdatedFName(fName);
+    setUpdatedFName(lName);
+    setUpdatedFName(email);
+  }
+
+  console.log(updatedFName)
+  console.log(updatedLName)
+  console.log(updatedEmail)
   return (
     <div className="container">
       <h1>
-        Hello {contact.fName} {contact.lName}
+        Hello {updatedFName} {updatedLName}
       </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+      <p>{updatedEmail}</p>
+      <form onSubmit={updatContact}>
+        <input name="fName" placeholder="First Name" onChange={updateContact} />
+        <input name="lName" placeholder="Last Name" onChange={updateContact} />
+        <input name="email" placeholder="Email" onChange={updateContact} />
         <button>Submit</button>
       </form>
     </div>
