d87b663d3e425ed20f6811a50e59445858242f9e
Harunzy <Harunzy55@gmail.com>
2026-04-22 13:35:22 -0700

    add solution

--- React-with-angela/316-es6-spread-operator-practice/src/components/App.jsx
+++ React-with-angela/316-es6-spread-operator-practice/src/components/App.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasksList, setTasksList] = useState([]);
+
+  function handleTaskChange(event) {
+    const newList = event.target.value;
+    setTask(newList);
+  }
+  function addTask() {
+    setTasksList((prevTasks) => {
+      return [...prevTasks, task];
+    });
+    setTask("");
+  }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={handleTaskChange} value={task.name} />
+        <button onClick={addTask}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {tasksList.map((task) => {
+            return <li key={task}>{task}</li>;
+          })}
         </ul>
       </div>
     </div>
