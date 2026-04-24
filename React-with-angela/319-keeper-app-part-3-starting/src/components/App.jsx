388286a4bed8dd3e0bd314573d4125deec169be8
Harunzy <Harunzy55@gmail.com>
2026-04-23 21:07:25 -0700

    add final solution

--- React-with-angela/319-keeper-app-part-3-starting/src/components/App.jsx
+++ React-with-angela/319-keeper-app-part-3-starting/src/components/App.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [noteItems, setNoteItems] = useState([]);
+  function addNote(note) {
+    setNoteItems((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setNoteItems((prevNotes) => {
+      return prevNotes.filter((noteItems, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+
+      {noteItems.map((noteItem, index) => {
+        return (
+          <Note
+            title={noteItem.title}
+            content={noteItem.content}
+            key={index}
+            id={index}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
--- React-with-angela/319-keeper-app-part-3-starting/src/components/CreateArea.jsx
+++ React-with-angela/319-keeper-app-part-3-starting/src/components/CreateArea.jsx
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  //handle input and textare
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  //handle form submit
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={note.title}
+        />
+        <textarea
+          name="content"
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+          onChange={handleChange}
+        />
+
+        <button onClick={submitNote}>
+          <span>Add</span>
+        </button>
       </form>
     </div>
   );
